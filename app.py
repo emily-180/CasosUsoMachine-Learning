543ffdf3b1369179de69da0f2b6403fa3f8e98e9
emily-180 <emily.moreira.8@gmail.com>
2025-09-01 13:03:13 -0500

    añandir trabajo base

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")  
+def home():
+    # redireciona para a rota "index"
+    return redirect(url_for("index"))
+
+@app.route("/index")
+def index():
+    myname = "Flask"
+    return render_template("index.html", myname=myname)
+
+if __name__ == "__main__":
+    app.run(debug=True)
